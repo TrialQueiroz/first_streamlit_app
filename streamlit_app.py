a26b39fb07be649b6b42d4dcc059b96996b80739
TrialQueiroz <127313673+TrialQueiroz@users.noreply.github.com>
2023-03-08 10:28:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-stremlit.markdown('Breakfast meno')
+stremlit.header('Breakfast meno')
 stremlit.subheader( 
   ' Omega 3 & Blueberry Oatmeal\n',
   'Kale, Spinach & Rocket `Smoothie\n',
