8d4a66b43b667347934404f79b704a021e90b660
TrialQueiroz <127313673+TrialQueiroz@users.noreply.github.com>
2023-03-08 10:47:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title('My parents New Healthy Diner')
 
-st.header('🥣Breakfast menu')
-st.text( ' Omega 3 & Blueberry Oatmeal')
+st.header('Breakfast menu')
+st.text( ' 🥣 Omega 3 & Blueberry Oatmeal')
 st.text( '🥗 Kale, Spinach & Rocket Smoothie')
 st.text( '🐔 Hard-Boiled Free-Range')
 st.text( '🥑🍞 Avocado Toast')
