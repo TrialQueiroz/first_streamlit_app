5264fb5c4ae6ac8e249439dbddf0563a3b07c2a6
TrialQueiroz <127313673+TrialQueiroz@users.noreply.github.com>
2023-03-08 10:31:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.markdown('Breakfast menu'),
+streamlit.markdown('Breakfast menu')
 streamlit.subheader( 
-  ' Omega 3 & Blueberry Oatmeal\n'
-  'Kale, Spinach & Rocket `Smoothie\n'
-  'Hard-Boiled Free-Range Egg'
-);
+  ' Omega 3 & Blueberry Oatmeal)
